b53634e0702118ebb4e049c183f9c6fc3a57197e
Marcelo <feltesmarcelo@gmail.com>
2025-05-14 18:39:18 -0300

    commit

--- src/components/pages/purchaseOrders/purchaseOrdersList/PurchaseOrderList.jsx
+++ src/components/pages/purchaseOrders/purchaseOrdersList/PurchaseOrderList.jsx
@@ -6,6 +6,7 @@ import { Icons } from "../../../../assets/Icons";
 import { currencyFormat } from "../../../common/currencyFormat/CurrencyFormatContainer";
 import { dateFormat, generalBackGroundColor } from "../../../../utils/helpers";
 import { Link } from "react-router-dom";
+import { PaginationContainer } from "../../../common/pagination/PaginationContainer";
 
 export const PurchaseOrderList = (purchaseOrdersListProps) => {
   const { filteredOrders, ...generalBarContainerProps } =
@@ -22,79 +23,86 @@ export const PurchaseOrderList = (purchaseOrdersListProps) => {
       <Box className="generalSubTitle">
         {filteredOrders.length} registros encontrados
       </Box>
-      <Box className="generalList">
-        {filteredOrders.map((order) => (
-          <Card sx={{ minWidth: 275, height: 300 }} key={order.id}>
-            <Box
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-                gap: 1,
-                justifyContent: "center",
-              }}
-            >
-              <Box className="itemStyle">
-                <Icons.ShoppingCartIcon sx={iconStyle} />
-                <b>Número: </b> {order.id}
-              </Box>
-              <Box className="itemStyle">
-                <Icons.PersonIcon sx={iconStyle} />
-                <b>Comprador: </b> {order.buyer_name} {order.buyer_last_name}
-              </Box>
-              <Box className="itemStyle">
-                <Icons.WhatsAppIcon sx={iconStyle} />
-                <b>Teléfono: </b>
-                <a
-                  href={`https://wa.me/${order.buyer_phone_number}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
+      <PaginationContainer items={filteredOrders} itemsPerPage={10}>
+        {(recordsToShow) => (
+          <Box className="generalList">
+            {recordsToShow.map((record) => (
+              <Card sx={{ minWidth: 275, height: 300 }} key={record.id}>
+                <Box
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 1,
+                    justifyContent: "center",
+                  }}
                 >
-                  {order.buyer_phone_number}
-                </a>
-              </Box>
-              <Box className="itemStyle">
-                <Icons.AlternateEmailIcon sx={iconStyle} />
-                <b>Email: </b>
-                <a href={`mailto:${order.buyer_email}`}>{order.buyer_email}</a>
-              </Box>
-              <Box className="itemStyle">
-                <Icons.CalendarMonthIcon sx={iconStyle} />
-                <b>Fecha: </b> {dateFormat(order.date)}
-              </Box>
-              <Box className="itemStyle">
-                <Icons.MonetizationOnIcon sx={iconStyle} />
-                <b>Total: </b>{" "}
-                <Box sx={{ marginBottom: "5px" }}>
-                  {currencyFormat(order.total_price)}
-                </Box>
-              </Box>
-              <Button
-                variant="outlined"
-                size="small"
-                // onClick={() => handleGetOrderDetails(order.id)}
-                component={Link}
-                to={`/purchaseOrders/details/${order.id}`}
-                sx={{
-                  border: "1px solid black",
-                  marginTop: "30px",
-                  width: "100%",
-                  color: "black",
-                  backgroundColor: "white",
+                  <Box className="itemStyle">
+                    <Icons.ShoppingCartIcon sx={iconStyle} />
+                    <b>Número: </b> {record.id}
+                  </Box>
+                  <Box className="itemStyle">
+                    <Icons.PersonIcon sx={iconStyle} />
+                    <b>Comprador: </b> {record.buyer_name}{" "}
+                    {record.buyer_last_name}
+                  </Box>
+                  <Box className="itemStyle">
+                    <Icons.WhatsAppIcon sx={iconStyle} />
+                    <b>Teléfono: </b>
+                    <a
+                      href={`https://wa.me/${record.buyer_phone_number}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {record.buyer_phone_number}
+                    </a>
+                  </Box>
+                  <Box className="itemStyle">
+                    <Icons.AlternateEmailIcon sx={iconStyle} />
+                    <b>Email: </b>
+                    <a href={`mailto:${record.buyer_email}`}>
+                      {record.buyer_email}
+                    </a>
+                  </Box>
+                  <Box className="itemStyle">
+                    <Icons.CalendarMonthIcon sx={iconStyle} />
+                    <b>Fecha: </b> {dateFormat(record.date)}
+                  </Box>
+                  <Box className="itemStyle">
+                    <Icons.MonetizationOnIcon sx={iconStyle} />
+                    <b>Total: </b>{" "}
+                    <Box sx={{ marginBottom: "5px" }}>
+                      {currencyFormat(record.total_price)}
+                    </Box>
+                  </Box>
+                  <Button
+                    variant="outlined"
+                    size="small"
+                    // onClick={() => handleGetOrderDetails(order.id)}
+                    component={Link}
+                    to={`/purchaseOrders/details/${record.id}`}
+                    sx={{
+                      border: "1px solid black",
+                      marginTop: "30px",
+                      width: "100%",
+                      color: "black",
+                      backgroundColor: "white",
 
-                  "&:active": {
-                    backgroundColor: generalBackGroundColor,
-                    color: "white",
-                    border: `1px solid white`,
-                  },
-                }}
-              >
-                Ver detalles
-              </Button>
-            </Box>
-          </Card>
-        ))}
-      </Box>
+                      "&:active": {
+                        backgroundColor: generalBackGroundColor,
+                        color: "white",
+                        border: `1px solid white`,
+                      },
+                    }}
+                  >
+                    Ver detalles
+                  </Button>
+                </Box>
+              </Card>
+            ))}
+          </Box>
+        )}
+      </PaginationContainer>
     </Box>
   );
 };
